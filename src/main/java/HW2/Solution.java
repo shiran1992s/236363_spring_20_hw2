5d99fcedb7712ebd94139faa385a94b07301d7ac
shiran1992s@gmail.com <47076788+shiran1992s@users.noreply.github.com>
2020-11-28 15:56:30 +0200

    Basic Create + Drop + Delete

--- src/main/java/HW2/Solution.java
+++ src/main/java/HW2/Solution.java
@@ -8,26 +8,176 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+
 import HW2.data.PostgreSQLErrorCodes;
+import javafx.util.Pair;
 
 import java.util.ArrayList;
 
 import static HW2.business.ReturnValue.*;
 
 
+
 public class Solution {
+
+    private static void createTable(String name, ArrayList<Pair<String, String>> typedSchema) {
+        Connection connection = DBConnector.getConnection();
+        PreparedStatement pstmt = null;
+
+        try {
+            StringBuilder string_builder = new StringBuilder();
+            string_builder.append("CREATE TABLE " + name + "\n" + "(\n");
+            for (Pair field : typedSchema)
+                string_builder.append("    " + field.getKey() + " " + field.getValue() + ",\n");
+            string_builder.deleteCharAt(string_builder.length()-2);             //delete last ","
+            string_builder.append(")");
+            pstmt = connection.prepareStatement(string_builder.toString());
+
+            pstmt.execute();
+
+        } catch (SQLException e) {
+//            e.printStackTrace();
+        } finally {
+            try {
+                pstmt.close();
+            } catch (SQLException e) {
+//                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+//                e.printStackTrace();
+            }
+        }
+    }
+
+
     public static void createTables() {
         InitialState.createInitialState();
         //create your tables here
+        ArrayList<Pair<String, String>> typedSchema = new ArrayList<>();
+        typedSchema.add(new Pair("course_id","integer NOT NULL"));
+        typedSchema.add(new Pair("semester","integer NOT NULL"));
+        typedSchema.add(new Pair("time","integer NOT NULL"));
+        typedSchema.add(new Pair("room","integer NOT NULL"));
+        typedSchema.add(new Pair("day","integer NOT NULL"));
+        typedSchema.add(new Pair("credit_points","integer NOT NULL"));
+        typedSchema.add(new Pair("PRIMARY KEY","(course_id)"));
+        typedSchema.add(new Pair("UNIQUE","(course_id)"));
+        typedSchema.add(new Pair("UNIQUE","(semester)"));
+        typedSchema.add(new Pair("CHECK","(course_id > 0)"));
+        typedSchema.add(new Pair("CHECK","(room > 0)"));
+        typedSchema.add(new Pair("CHECK","(credit_points > 0)"));
+        createTable("Test", typedSchema);
+        typedSchema.clear();
+
+        typedSchema.add(new Pair("supervisor_id","integer NOT NULL"));
+        typedSchema.add(new Pair("supervisor_name","char[] NOT NULL"));
+        typedSchema.add(new Pair("salary","integer NOT NULL"));
+        typedSchema.add(new Pair("PRIMARY KEY","(supervisor_id)"));
+        typedSchema.add(new Pair("UNIQUE","(supervisor_id)"));
+        typedSchema.add(new Pair("CHECK","(supervisor_id > 0)"));
+        typedSchema.add(new Pair("CHECK","(salary >= 0)"));
+        createTable("Supervisor", typedSchema);
+        typedSchema.clear();
+
+        typedSchema.add(new Pair("student_id","integer NOT NULL"));
+        typedSchema.add(new Pair("student_name","char[] NOT NULL"));
+        typedSchema.add(new Pair("faculty","char[] NOT NULL"));
+        typedSchema.add(new Pair("credit_points","integer NOT NULL"));
+        typedSchema.add(new Pair("PRIMARY KEY","(student_id)"));
+        typedSchema.add(new Pair("UNIQUE","(student_id)"));
+        typedSchema.add(new Pair("CHECK","(student_id > 0)"));
+        typedSchema.add(new Pair("CHECK","(credit_points >= 0)"));
+        createTable("Student", typedSchema);
+        typedSchema.clear();
     }
 
     public static void clearTables() {
         //clear your tables here
+        Connection connection = DBConnector.getConnection();
+        PreparedStatement pstmt = null;
+        try {
+            pstmt = connection.prepareStatement("SELECT table_name\n" +
+                    "  FROM information_schema.tables\n" +
+                    "  WHERE table_schema='public'\n" +
+                    "   AND table_type='BASE TABLE';");
+            ResultSet resultSet = pstmt.executeQuery();
+
+            while (resultSet.next())
+            {
+                String tableName = resultSet.getString("table_name");
+                pstmt = connection.prepareStatement("DELETE FROM  " + tableName + " CASCADE");
+                pstmt.execute();
+            }
+            resultSet.close();
+        } catch (SQLException e) {
+            //e.printStackTrace()();
+        }
+        finally {
+            try {
+                pstmt.close();
+            } catch (SQLException e) {
+                //e.printStackTrace()();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                //e.printStackTrace()();
+            }
+        }
     }
 
     public static void dropTables() {
         InitialState.dropInitialState();
 		//drop your tables here
+        Connection connection = DBConnector.getConnection();
+        PreparedStatement pstmt = null;
+        try {
+            // drop views;
+            pstmt = connection.prepareStatement("SELECT table_name\n" +
+                    "  FROM information_schema.tables\n" +
+                    "  WHERE table_schema='public'\n" +
+                    "   AND table_type='VIEW';");
+            ResultSet resultSet1 = pstmt.executeQuery();
+
+            while (resultSet1.next())
+            {
+                String viewName = resultSet1.getString("table_name");
+                pstmt = connection.prepareStatement("DROP VIEW IF EXISTS " + viewName + " CASCADE");
+                pstmt.execute();
+            }
+            resultSet1.close();
+
+            //drop tables;
+            pstmt = connection.prepareStatement("SELECT table_name\n" +
+                    "  FROM information_schema.tables\n" +
+                    "  WHERE table_schema='public'\n" +
+                    "   AND table_type='BASE TABLE';");
+            ResultSet resultSet2 = pstmt.executeQuery();
+
+            while (resultSet2.next())
+            {
+                String tableName = resultSet2.getString("table_name");
+                pstmt = connection.prepareStatement("DROP TABLE IF EXISTS " + tableName + " CASCADE");
+                pstmt.execute();
+            }
+            resultSet2.close();
+        } catch (SQLException e) {
+            //e.printStackTrace()();
+        }
+        finally {
+            try {
+                pstmt.close();
+            } catch (SQLException e) {
+                //e.printStackTrace()();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                //e.printStackTrace()();
+            }
+        }
     }
 
     public static ReturnValue addTest(Test test) {
